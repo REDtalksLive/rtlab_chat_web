ce002f1f043f4288fdca1818bb790a84b55489b6
REDtalks.live <63261751+REDtalksLive@users.noreply.github.com>
2021-11-15 20:47:28 -0800

    Exit if db unreachable.

--- src/mongodb.js
+++ src/mongodb.js
@@ -4,7 +4,10 @@ module.exports = function (app) {
   const connection = app.get('mongodb');
   const database = connection.substr(connection.lastIndexOf('/') + 1);
   const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true })
-    .then(client => client.db(database));
+    .then(client => client.db(database))
+    .catch(err => {
+      process.exit(1);
+    });
 
   app.set('mongoClient', mongoClient);
 };
